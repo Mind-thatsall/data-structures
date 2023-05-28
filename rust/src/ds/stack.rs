a4f29ffaf7e0c71845ecbd01eed649764e8684d7
Mind-thatsall <mathieurossi77@gmail.com>
2023-05-28 23:46:38 +0200

    changed structure of file

---
+++ rust/src/ds/stack.rs
@@ -0,0 +1,41 @@
+struct Stack<T> {
+    items: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Stack<T> {
+        Stack { items: Vec::new() }
+    }
+
+    fn push(&mut self, value: T) {
+        self.items.push(value);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+
+    fn length(&self) -> usize {
+        self.items.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.items.len() == 0
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.items.last()
+    }
+}
+
+pub fn run() {
+    let mut stack: Stack<i8> = Stack::new();
+
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.pop();
+    dbg!(stack.peek());
+    dbg!(stack.is_empty());
+    dbg!(stack.length());
+}
